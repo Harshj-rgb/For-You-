e6c94c14935bcf8868ac4fdbe1ef42fd49345fa4
Harsh Jagadale <78469151+Harshj-rgb@users.noreply.github.com>
2026-07-30 16:19:09 +0530

    Manuscript

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const VIDEO_FILE = 'Cute.mp4';       // ← Your final video
 // ==========================================
 // PASSWORD CONFIGURATION
 // ==========================================
-const CORRECT_PASSWORD = "Bunty"; // ← Change to your password
+const CORRECT_PASSWORD = "bunty"; // ← Change to your password
 // ==========================================
 
 
